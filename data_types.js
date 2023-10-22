2a9dd0e69e5ddf78a12419423d6e231782d2ee7c
prafullpandey614 <prafullpandey614@gmail.com>
2023-10-22 14:56:55 +0530

    data types

---
+++ data_types.js
@@ -0,0 +1,30 @@
+// primitive data types
+/*
+1. number 
+2. string
+3. boolean
+4. symbol
+5. bigInt
+6. undefined typeof --> (undefined)
+7. null typeof --> (obj)
+*/
+
+let age = 23 
+let names = "nil young"
+let isLoggedIn = false
+let id = Symbol(names)
+let accountBalance = 987654321987654321987654321987654321n;
+let girlFriend = undefined ;
+let haisiyat = null ;
+
+console.table([age,names,isLoggedIn,id,accountBalance,girlFriend,haisiyat])
+console.table([typeof(age),typeof(names),typeof(isLoggedIn),typeof(id),typeof(accountBalance),typeof(girlFriend),typeof(haisiyat)])
+
+// Non primitive data types
+/* 
+1. object
+2. array
+3. function
+4. date
+*/
+
