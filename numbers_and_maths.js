5b6ed9bf47703b848ab65be54aa83465ca8490c1
prafullpandey614 <prafullpandey614@gmail.com>
2023-10-29 15:25:42 +0530

    basic math functions

---
+++ numbers_and_maths.js
@@ -0,0 +1,9 @@
+
+let age  = new Number(56.56423)
+
+console.log(age.toFixed(3))
+console.log(age.toPrecision(4).toLocaleString()) // round off
+console.log(Math.ceil(age)) //57
+console.log(Math.floor(age)) //56
+console.log(Math.floor(Math.random()*10+1)) // random() function return a random number between 0 and 1
+
